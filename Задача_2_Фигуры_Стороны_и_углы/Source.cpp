bcbe74d744fa40f978c0319156d5c4ab24a14344
EnotBog <enotbog@gmail.com>
2022-06-13 21:51:01 +0300

    the access modifier has been edited

--- Задача_2_Фигуры_Стороны_и_углы/Source.cpp
+++ Задача_2_Фигуры_Стороны_и_углы/Source.cpp
@@ -15,12 +15,13 @@ public:
 
 	Figure() { sides_count = 0; this->name = "Фигура:"; };
 
-	
-	
-	virtual void print_info() 
+
+
+	virtual void print_info()
 	{
 		std::cout << get_name() << "\n " << get_corn() << get_sides() << "" << "\n";//
 	}
+
 
 private:
 
@@ -55,13 +56,15 @@ class Triangle : public Figure //треугольник скц
 {
 public:
 	Triangle() { sides_count = 3; }
-	Triangle(int a,int b,int c, int A, int B, int C) 
+	Triangle(int a, int b, int c, int A, int B, int C)
 	{
-		this->a = a; this->b = b; this->c = c; 
-		this->A = A; this->B = B; this->C = C; 
+		this->a = a; this->b = b; this->c = c;
+		this->A = A; this->B = B; this->C = C;
 		this->name = "Треугольник:";
+		this->sides_count = 3;
 	};
 
+protected:
 	std::string get_sides()
 	{
 		std::string buf = { "Стороны: a=" + std::to_string(a) + " b=" + std::to_string(b) + " c=" + std::to_string(c) + "\n" };
@@ -75,22 +78,21 @@ public:
 
 	void print_info() override
 	{
-		std::cout << Triangle::get_name() << "\n"  << Triangle::get_sides() << Triangle::get_corn() << "" << " " << " " << "\n"; // 
+		std::cout << Triangle::get_name() << "\n" << Triangle::get_sides() << Triangle::get_corn() << "" << " " << " " << "\n"; // 
 	}
-	
 
-protected:
-	int a=0, b=0, c=0;
-	int A=0, B=0, C=0;
+
+	int a = 0, b = 0, c = 0;
+	int A = 0, B = 0, C = 0;
 
 private:
 
 };
 
-class Equilateral_triangle:public Triangle //равносторонний треугольник
+class Equilateral_triangle :public Triangle //равносторонний треугольник
 {
 public:
-	Equilateral_triangle(int a):Triangle( a,a,a,60,60,60)
+	Equilateral_triangle(int a) :Triangle(a, a, a, 60, 60, 60)
 	{
 		this->name = "Равносторонний треугольник: ";
 	}
@@ -99,10 +101,10 @@ private:
 };
 
 
-class Isosceles_triangle:public Triangle // равнобедренный треугольник
+class Isosceles_triangle :public Triangle // равнобедренный треугольник
 {
 public:
-	Isosceles_triangle(int a,int c,int A,int B):Triangle(a,a,c,A,B,A)
+	Isosceles_triangle(int a, int c, int A, int B) :Triangle(a, a, c, A, B, A)
 	{
 		this->name = "Равнобедренный треугольник: ";
 	}
@@ -111,10 +113,10 @@ private:
 };
 
 
-class Rectangular_triangle:public Triangle //прямоугольный треугольник
+class Rectangular_triangle :public Triangle //прямоугольный треугольник
 {
 public:
-	Rectangular_triangle(int a, int b, int c, int A, int B):Triangle(a,b,c,A,B,90)
+	Rectangular_triangle(int a, int b, int c, int A, int B) :Triangle(a, b, c, A, B, 90)
 	{
 		this->name = "Прямоугольный треугольник: ";
 	}
@@ -127,15 +129,17 @@ private:
 class Quadrangle : public Figure
 {
 public:
-	Quadrangle() { sides_count = 4;};
+	Quadrangle() { };
 
 	Quadrangle(int a, int b, int c, int d, int A, int B, int C, int D)
 	{
 		this->a = a; this->b = b; this->c = c; this->d = d;
 		this->A = A; this->B = B; this->C = C; this->D = D;
 		this->name = "Четырехугольник:";
+		this->sides_count = 4;
 	}
 
+protected:
 	std::string get_sides()
 	{
 		std::string buf = { "Стороны: a=" + std::to_string(a) + " b=" + std::to_string(b) + " c=" + std::to_string(c) + " d=" + std::to_string(d) + "\n" };
@@ -148,10 +152,9 @@ public:
 	}
 	void print_info() override
 	{
-		std::cout << Quadrangle::get_name() << "\n"  << Quadrangle::get_sides() << Quadrangle::get_corn() << "" << "\n"; // 
+		std::cout << Quadrangle::get_name() << "\n" << Quadrangle::get_sides() << Quadrangle::get_corn() << "" << "\n"; // 
 	}
 
-protected:
 
 	int a = 0, b = 0, c = 0, d = 0;
 	int A = 0, B = 0, C = 0, D = 0;
@@ -169,7 +172,7 @@ public:
 		this->name = "Параллелограм: ";
 	}
 
-	
+
 private:
 
 };
@@ -177,7 +180,7 @@ private:
 class My_Rectangle : public Parallelogram // Прямоугольник
 {
 public:
-	My_Rectangle(int a, int b) :Parallelogram(a, b, 90, 90) 
+	My_Rectangle(int a, int b) :Parallelogram(a, b, 90, 90)
 	{
 		this->name = "Прямоугольник: ";
 	}
@@ -197,7 +200,7 @@ public:
 private:
 };
 
-class Rhomb: public Parallelogram //ромб
+class Rhomb : public Parallelogram //ромб
 {
 public:
 	Rhomb(int a, int A, int B) : Parallelogram(a, a, A, B)
@@ -219,18 +222,18 @@ int main()
 	SetConsoleOutputCP(1251);
 
 	Figure f = {};
-	Triangle triagle = {10,20,30,50,60,70};
-	Rectangular_triangle r_triagle = {10,20,30,50,60};
-	Isosceles_triangle i_triagle = { 10,20,10,60};
+	Triangle triagle = { 10,20,30,50,60,70 };
+	Rectangular_triangle r_triagle = { 10,20,30,50,60 };
+	Isosceles_triangle i_triagle = { 10,20,10,60 };
 	Equilateral_triangle e_triagle = { 10 };
 
-	Quadrangle quadrangle = {10,20,30,40,50,60,70,80}; // четырех угольник
+	Quadrangle quadrangle = { 10,20,30,40,50,60,70,80 }; // четырех угольник
 	My_Rectangle rectangle = { 10,20 };//прямоугольник
 	Square square = { 20 }; // квадрат
 	Parallelogram parallelogram = { 20,30,30,40 }; //параллелограмм
 	Rhomb rhomb = { 30,30,40 }; // ромб
 
-	
+
 	Figure* p_triagle = &triagle;
 	Figure* p_equilateral_triangle = &e_triagle;
 	Figure* p_isosceles_triangle = &i_triagle;
@@ -241,16 +244,18 @@ int main()
 	Figure* p_square = &square;
 	Figure* p_parallelogram = &parallelogram;
 	Figure* p_rhomb = &rhomb;
-  
-p_triagle->print_info();
-p_rectangular_triangle->print_info();
-p_isosceles_triangle->print_info();
-p_equilateral_triangle->print_info();
 
-p_quadrangle->print_info();
-p_rectangle->print_info();
-p_square->print_info();
-p_parallelogram->print_info();
-p_rhomb->print_info();
+	p_triagle->print_info();
+	p_rectangular_triangle->print_info();
+	p_isosceles_triangle->print_info();
+	p_equilateral_triangle->print_info();
+
+	p_quadrangle->print_info();
+	p_rectangle->print_info();
+	p_square->print_info();
+	p_parallelogram->print_info();
+	p_rhomb->print_info();
+
+
 	return 0;
 }
